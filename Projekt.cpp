c4edebab45d2869890f6c96c400ef2bb40f034c6
Marusz <maruszmalaga001@gmail.com>
2025-10-27 13:20:29 +0100

    zmiany w Projekt.cpp by był main

--- Projekt.cpp
+++ Projekt.cpp
@@ -1,7 +1,32 @@
 ﻿#include <iostream>
+#include "ListFactory.h"
 using namespace std;
-int main()
-{
-	cout << "Hello World!\n";
-}
 
+int main() {
+    DoublyLinkedList* list = ListFactory::createList();
+
+    list->addBack(10);
+    list->addFront(5);
+    list->addBack(20);
+    list->addAt(1, 15);
+
+    list->display();
+    list->displayReverse();
+
+    list->removeFront();
+    list->removeBack();
+    list->removeAt(0);
+
+    list->display();
+
+    // Iterator test
+    Iterator it = list->getIteratorFromStart();
+    cout << "Następny element: " << it.next() << endl;
+    cout << "Poprzedni element: " << it.prev() << endl;
+
+    list->clear();
+    list->display();
+
+    delete list;
+    return 0;
+}
