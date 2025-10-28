c736cf99cf5ef49a5dde15f8633d337c8830fd39
Marusz <maruszmalaga001@gmail.com>
2025-10-28 17:00:35 +0100

    dodanie komentarza

--- ListFactory.h
+++ ListFactory.h
@@ -3,8 +3,13 @@
 
 #include "DoublyLinkedList.h"
 
+// Klasa ListFactory implementuje wzorzec projektowy "Factory" (Fabryka)
+// Jej zadaniem jest tworzenie obiektów klasy DoublyLinkedList.
+// Dziêki temu kod w main() jest prostszy, a sposób tworzenia listy
+// mo¿na ³atwo zmieniæ w przysz³oœci (np. na listê z innym typem danych).
 class ListFactory {
 public:
+    // Metoda tworz¹ca nowy obiekt listy dwukierunkowej
     static DoublyLinkedList* createList(); 
 };
 
